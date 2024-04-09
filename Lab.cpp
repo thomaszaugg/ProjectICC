d5c523c40b27dd50726b7013f9bd1da8ec70b2df
thomaszaugg <164908947+thomaszaugg@users.noreply.github.com>
2024-04-09 10:49:22 +0200

    Add files via upload

---
+++ Lab.cpp
@@ -0,0 +1,137 @@
+#include "Lab.hpp"
+#include "Application.hpp"
+#include <Utility/Utility.hpp>
+#include "Hamster.hpp"
+#include "Pellets.hpp"
+
+unsigned int Lab::maxCageNumber(){
+    double min_size(getAppConfig().simulation_lab_min_box_size);
+    if(min_size < 2*getAppConfig().hamster_size) {
+        min_size=2*getAppConfig().hamster_size;
+    }
+    unsigned int maxNumber(getAppConfig().simulation_lab_size/min_size);
+    if (maxNumber < 1){
+        maxNumber=1;
+    }
+
+    return maxNumber;
+}
+
+void Lab::CageNumberCheck(unsigned int& nbCagesPerRow){
+    if(maxCageNumber() < nbCagesPerRow){
+        nbCagesPerRow=maxCageNumber();
+    }
+    if(nbCagesPerRow==0){
+        throw std::invalid_argument("Zero Cages not possible");
+    }
+}
+
+void Lab::makeBoxes(unsigned int nbCagesPerRow){
+    cages.clear();
+    CageNumberCheck(nbCagesPerRow);
+    double widthBox(getAppConfig().simulation_lab_size/nbCagesPerRow);
+    std::vector<Cage*> vCage;
+    for (unsigned int i(0); i < nbCagesPerRow; ++i){
+        vCage.clear();
+        for (unsigned int j(0); j < nbCagesPerRow; ++j){     //assumption: empty vector
+            Vec2d position (widthBox/2 + i*widthBox, widthBox/2 + j*widthBox);
+            Cage* ptr(new Cage(position, widthBox, widthBox , 0.025*widthBox));
+            vCage.push_back(ptr);
+        }
+        cages.push_back(vCage);
+    }
+}
+
+
+Lab::Lab(){
+    makeBoxes(getAppConfig().simulation_lab_nb_boxes); //does the argument has to be in the constructer
+}
+
+unsigned int Lab::getNbCagesPerRow(){
+    return cages.size();
+}
+
+
+void Lab::addCageToRow(){
+    unsigned int newNumber(cages.size()+1);
+    CageNumberCheck(newNumber);
+
+    clearCages();
+    makeBoxes(newNumber);
+
+}
+
+void Lab::removeCageFromRow(){
+    unsigned int newNumber(cages.size()-1);
+    CageNumberCheck(newNumber);
+
+    clearCages();
+    makeBoxes(newNumber);
+
+}
+
+void Lab::update(sf::Time dt){
+    //change the contents of the cages over time
+}
+
+void Lab::drawOn(sf::RenderTarget& targetWindow){
+    drawOnCages(targetWindow);
+
+    if(hamster!=nullptr) hamster->drawOn(targetWindow);
+    if(pellet!=nullptr) pellet->drawOn(targetWindow);
+}
+
+void Lab::drawOnCages(sf::RenderTarget& targetWindow){
+
+    for (auto& row: cages){
+        for (auto& ele: row){
+            (*ele).drawOn(targetWindow);
+        }
+    }
+}
+
+void Lab::reset(bool reset){
+    if (reset){
+        clearCages();
+        makeBoxes(getAppConfig().simulation_lab_nb_boxes);
+    }else{
+     clearEntities();
+    }
+}
+
+void Lab::clearEntities(){
+    delete hamster;
+    hamster=nullptr;
+
+    delete pellet;
+    pellet=nullptr;
+}
+void Lab::clearCages(){
+    clearEntities();
+    for (unsigned int i(0); i < cages.size(); ++i){
+        for (unsigned int j(0); j < cages.size(); ++j){
+            delete cages[i][j];
+        }
+        cages[i].clear();
+    }
+    cages.clear();
+}
+
+Lab::~Lab(){
+    clearCages();
+};
+
+bool Lab::addAnimal(Hamster* h){
+     if(h!=nullptr and hamster==nullptr){
+         hamster=h;
+         return true;}
+
+         return false;
+ }
+
+ bool Lab::addFood(Pellets* p){
+ if(pellet==nullptr and p!=nullptr){
+     pellet=p;
+     return true;}
+ return false;
+ }
---
+++ Lab.hpp
@@ -0,0 +1,108 @@
+#ifndef LAB_HPP
+#define LAB_HPP
+#include <vector>
+#include "Cage.hpp"
+#include <memory>
+#include "SFML/Graphics/RenderTarget.hpp"
+
+class Pellets;
+class Hamster;
+
+typedef std::vector<std::vector<Cage*>> Cages;
+
+
+
+class Lab
+{
+private:
+    Cages cages;
+   Pellets* pellet = nullptr;
+   Hamster* hamster = nullptr;
+
+    /*!
+    * @brief Check whether the value nbCagesPerRow is inbetween the min and max barrier
+    * if not, the function does also correct the value
+    */
+    void CageNumberCheck(unsigned int& nbCagesPerRow);
+
+    /*!
+    * @brief deletes all the cages or all the entities
+    */
+    void clearCages();
+    void clearEntities();
+    /*!
+    * @brief Helper function that draws the Cages of the lab
+    */
+    void drawOnCages(sf::RenderTarget& targetWindow);
+
+
+public:
+    /*!
+    * @brief default constructor
+    */
+    Lab();
+
+    /*!
+    * @brief defines the maxCageNumber
+    */
+    unsigned int maxCageNumber();
+
+    /*!
+    * @brief constructs the different cages
+    */
+    void makeBoxes(unsigned int nbCagesPerRow);
+
+    /*!
+    * @brief Getter for attribut nbCagesPerRow
+    */
+    unsigned int getNbCagesPerRow();
+
+    /*!
+    * @brief this function increases the nbCagesPerRow by one (if possible under the constraint)
+    * and reinitializes the boxes with the new number
+    */
+    void addCageToRow();
+
+    /*!
+    * @brief this function decreases the nbCagesPerRow by one (if possible under the constraint
+    * and reinitialises the boxes with the new number
+    */
+    void removeCageFromRow();
+
+    /*!
+    * @brief change the contents of the cages over time
+    */
+    void update(sf::Time dt);
+
+    /*!
+    * @brief Draws the lab
+    */
+    void drawOn(sf::RenderTarget& targetWindow);
+
+    /*!
+    * @brief partially or totaly reset the lab, if reset = false, only the content of the cages is deleted,
+    * otherwise content and cages are deleted
+    */
+    void reset(bool reset = false);
+
+    /*!
+    * @brief deconstructer
+    */
+    ~Lab();
+
+    /*!
+    * @brief to make it impossible to copy a Lab
+    */
+   Lab(const Lab&)=delete;
+
+    /*!
+    * @brief add hamsters and pellets
+    */
+   bool addAnimal(Hamster* h);
+   bool addFood(Pellets* p);
+};
+
+#endif // LAB_HPP
+
+
+
