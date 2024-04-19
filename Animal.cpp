560017a165fbc1244855c9ca369ef4acf1bdf92e
thomaszaugg <164908947+thomaszaugg@users.noreply.github.com>
2024-04-19 14:18:14 +0200

    Add files via upload

--- Animal.cpp
+++ Animal.cpp
@@ -9,7 +9,7 @@ std::vector<double> probabilities = {0.0000,0.0000,0.0005,0.0010,0.0050,0.9870,0
 
 
 Animal::Animal(const Vec2d& position, double energy)
-    : Entity(position, energy){
+    : Entity(position, energy), speed(0){
     }
 
 Animal::~Animal() {
@@ -38,7 +38,7 @@ void Animal::updateState(sf::Time dt){
 void Animal::move(sf::Time dt){
   //change of orientation
     counter+=dt;
-    if(counter > sf::seconds(getAppConfig().animal_rotation_delay)){    //since seconds are floats, we use comparison instead of egality
+    if(counter >= sf::seconds(getAppConfig().animal_rotation_delay)){    //since seconds are floats, we use comparison instead of egality
         setOrientation(getOrientation()+getNewRotation());              //energy can become
     }
   //look into angle delta
@@ -47,6 +47,7 @@ void Animal::move(sf::Time dt){
     Vec2d step=getSpeedVector()*dt.asSeconds();
         if(inCollision(step+getCenter())){
             setOrientation((-getHeading()).angle());
+            counter=sf::seconds(getAppConfig().animal_rotation_delay);              // to prevent glitching at the wall
         }
     updatePosition(step);
 
